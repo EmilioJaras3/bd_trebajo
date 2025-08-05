26309c5ca1b80abc72eca049d111150c0af79287
EmilioJaras3 <jarase01@gmail.com>
2025-08-05 03:34:49 -0600

    Add files via upload

---
+++ template_api_Examen/src/main/java/org/alilopez/repository/UserRepository.java
@@ -0,0 +1,87 @@
+package org.alilopez.repository;
+
+import org.alilopez.config.DatabaseConfig;
+import org.alilopez.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+    public List<User> findAll() throws SQLException {
+        List<User> users = new ArrayList<>();
+        String query = "SELECT * FROM users";
+        try (
+                Connection conn = DatabaseConfig.getDataSource().getConnection();
+                PreparedStatement stmt = conn.prepareStatement(query);
+                ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                User u = new User();
+                u.setId(rs.getInt("id"));
+                u.setName(rs.getString("name"));
+                u.setEmail(rs.getString("email"));
+                users.add(u);
+            }
+        }
+        return users;
+    }
+
+    public User findByIdUser(int idUser) throws SQLException {
+        User user = null;
+        String query = "SELECT * FROM users WHERE id = ?";
+
+        try (Connection conn = DatabaseConfig.getDataSource().getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+
+            stmt.setInt(1, idUser);
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    user = new User();
+                    user.setId(rs.getInt("id"));
+                    user.setName(rs.getString("name"));
+                    user.setEmail(rs.getString("email"));
+                }
+            }
+        }
+
+        return user;
+    }
+
+    public void save(User user) throws SQLException {
+        String query = "INSERT INTO users (name, email) VALUES (?, ?)";
+        try (Connection conn = DatabaseConfig.getDataSource().getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, user.getName());
+            stmt.setString(2, user.getEmail());
+            stmt.executeUpdate();
+        }
+    }
+
+    // Actualizar usuario
+    public void update(User user) throws SQLException {
+        String query = "UPDATE users SET name = ?, email = ? WHERE id = ?";
+        try (Connection conn = DatabaseConfig.getDataSource().getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, user.getName());
+            stmt.setString(2, user.getEmail());
+            stmt.setInt(3, user.getId());
+            stmt.executeUpdate();
+        }
+    }
+
+    // Eliminar usuario
+    public void delete(int id) throws SQLException {
+        String query = "DELETE FROM users WHERE id = ?";
+        try (Connection conn = DatabaseConfig.getDataSource().getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
+    }
+
+
+}
